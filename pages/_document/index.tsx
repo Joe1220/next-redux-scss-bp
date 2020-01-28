335f13fe634659d3f0fb847b969c8421283262af
joe <jsh901220@gmail.com>
2020-01-29 01:38:16 +0900

    [dev: style] - [add] basic font family chnage

--- pages/_document/index.tsx
+++ pages/_document/index.tsx
@@ -12,6 +12,10 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
